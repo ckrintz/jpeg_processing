68694f9c4c1a821f4e15c55aa06fc85006713682
Chandra Krintz <ckrintz@yahoo.com>
2016-05-19 12:58:29 -0700

    minor

--- process_output_files/dbiface.py
+++ process_output_files/dbiface.py
@@ -9,6 +9,7 @@ class DBobj(object):
         conn: A database connection
     '''
 
+    #constructor of a DBobj object
     def __init__(self, dbname, host='localhost'):
         args = "dbname='{0}' user='postgres' host='{1}'".format(dbname,host)
         try:
@@ -18,6 +19,7 @@ class DBobj(object):
             print 'Problem connecting to DB'
             sys.exit(1)
     
+    #invoke an SQL query on the db
     def execute_sql(self,sql):
         cur = self.conn.cursor()
         try:
@@ -29,40 +31,36 @@ class DBobj(object):
             sys.exit(1)
         return cur
 
-    def commit(self):
-        return self.conn.commit()
-
-    def getCursor(self):
-        return self.conn.cursor()
-
-    def importCSVwHeader(self,fname,tname): #CSV file has a header line/row
+    #import a CVS file into a table (empty the table first)
+    #we expect that the caller has created the table already with the right schema
+    #the table schema must match the CSV columns exactly or there will be error
+    def importCSV(self,fname,tname,hasHeader=True,overwrite=False): 
 	#CSV must match db schema exactly
         cur = self.conn.cursor()
         copy_sql = ''
         try:
-	    #delete the table if it exists
-            cur.execute("DELETE FROM {0};".format(tname))
-            cur.execute("SET CLIENT_ENCODING TO 'LATIN1';")
-            copy_sql = "COPY {0} FROM stdin WITH CSV HEADER DELIMITER as ',';".format(tname)
+            if overwrite:
+	        #delete the table if it exists
+                cur.execute("DELETE FROM {0};".format(tname))
+
+	    #account for header if there is one
+	    #http://www.postgresql.org/docs/9.2/static/sql-copy.html
+            if hasHeader:
+                copy_sql = "COPY {0} FROM STDIN WITH CSV HEADER DELIMITER as ',';".format(tname)
+	    else:
+                copy_sql = "COPY {0} FROM STDIN WITH ENCODING 'latin1';".format(tname)
+                #copy_sql = "COPY {0} FROM STDIN WITH DELIMITER ',' ENCODING 'latin1';".format(tname)
+
+	    #avoid getting utf8 errors reading in data
+            cur.execute("SET CLIENT_ENCODING TO 'latin1';")
+
+	    #read in the file
             with open(fname, 'r') as f:
                 cur.copy_expert(sql=copy_sql, file=f)
                 self.conn.commit()
         except Exception as e:
 	    print e
-            print 'importCSVwHeader: SQL problem:\n\t{0}'.format(copy_sql)
-            sys.exit(1)
-
-    def appendCSV(self,fname,tname): #CSV file has no header
-	#CSV must match db schema exactly
-        cur = self.conn.cursor()
-        try: 
-            with open(path, 'r') as f:
- 	            #disallows adding repeat keys
-                cur.copy_from(f, tname, sep=',')
-                self.conn.commit()
-        except Exception as e:
-	    print e
-            print 'appendCSV: SQL problem (copy_from)'
+            print 'importCSV: SQL problem:\n\t{0}'.format(copy_sql)
             sys.exit(1)
 
     def closeConnection(self):
--- process_output_files/process.py
+++ process_output_files/process.py
@@ -1,11 +1,5 @@
 '''Author: Chandra Krintz, UCSB, ckrintz@cs.ucsb.edu, AppScale BSD license'''
 
-'''csv file format:
-box_path:filename,date,time,ID,size,temp,flash,bad_temp,orig_fname
-Windmill\2015\11\08:Windmill_2015-11-08_23:08:55_091.JPG,2015-11-08,23:08:55,091,1004421,?,Flash,True,IMAG0091.JPG
-...
-'''
-
 import csv
 import json, sys, argparse, csv, logging, os, time
 import numpy as np
@@ -21,7 +15,8 @@ def main():
     parser.add_argument('fname',action='store',help='CSV file to import into DB (overwrite table)')
     parser.add_argument('outfname',action='store',help='text file to output for plotting')
     #optional arguments
-    parser.add_argument('--adddata',action='store_true',default=False,help='append CSV to db table as no-header data')
+    parser.add_argument('--overWrite',action='store_true',default=False,help='overwrite db table with data in this CSV (default is to append)')
+    parser.add_argument('--hasHeader',action='store_true',default=False,help='CSV has header row (default is has no header)')
     parser.add_argument('--debug',action='store_true',default=False,help='Turn debugging on')
     args = parser.parse_args()
 
@@ -32,13 +27,18 @@ def main():
     dbname = 'wtbdb'
     tname = 'metainfo'
 
-    #CSV header and example key/value pairs in a row
-    #box_path:filename,date,time,ID,size,temp,flash,bad_temp,orig_fname
-    #{'temp': '51', 'flash': 'NoFlash', 'bad_temp': 'False', 'box_path:filename': 'Bone\\2015\\09\\16:BoneH_2015-09-16_22:30:48_1499.JPG', 'time': '22:30:48', 'date': '2015-09-16', 'orig_fname': 'RCNX1499.JPG', 'ID': '1499', 'size': '1700891'}
-    #create a table with the same fields/types as the CSV so that we can do one big import
-    #temp is text b/c it may contain a ? when there is no temperature 
-    #    in the picture or when OCR fails to extract it
-    sql = "CREATE TABLE IF NOT EXISTS {0}(boxfname TEXT, dt DATE, ti TIME, pid INT, size INT, temp TEXT, flash TEXT, badtemp BOOL, origfname TEXT);".format(tname)
+    ''' 
+    The following creates a table with a schema that matches the CSV format:
+    	Header box_path:filename,date,time,ID,size,temp,flash,bad_temp,orig_fname
+    	key/value pairs {'temp': '51', 'flash': 'NoFlash', 'bad_temp': 'False', 'box_path:filename': 'Bone\\2015\\09\\16:BoneH_2015-09-16_22:30:48_1499.JPG', 'time': '22:30:48', 'date': '2015-09-16', 'orig_fname': 'RCNX1499.JPG', 'ID': '1499', 'size': '1700891'}
+    temp is text b/c it may contain a ? when there is no temperature 
+        in the picture or when OCR fails to extract it
+    For info on CONSTRAINT and creating primary keys from multiple columns see:
+    http://www.techonthenet.com/postgresql/primary_keys.php
+
+    SQL should be in double quotes so that single quotes can be used for internal/nested strings
+    '''
+    sql = "CREATE TABLE IF NOT EXISTS {0}(boxfname TEXT, dt DATE, ti TIME, pid INT, size INT, temp TEXT, flash TEXT, badtemp BOOL, origfname TEXT, CONSTRAINT pk PRIMARY KEY (dt, ti, pid));".format(tname)
     db = dbiface.DBobj(dbname)
     db.execute_sql(sql)
 
@@ -51,15 +51,13 @@ def main():
         for row in rows:
             print row
     
-    if args.adddata: #append
-        db.appendCSV(args.fname,tname)
-    else: #overwrite (deletes all data in the db first)
-        db.importCSVwHeader(args.fname,tname)
+    db.importCSV(args.fname,tname,args.hasHeader,args.overWrite)
 
     #read what is in the db after the import - use single quotes nested in outer double quotes
     #because SQL requires single
-    sql = "SELECT * FROM {0} WHERE dt > '2015-01-01' AND dt < '2015-05-23';".format(tname)
-    #other examples to try
+    sql = "SELECT * FROM {0} WHERE dt > '2015-11-08';".format(tname)
+    #other examples to try:
+    #sql = "SELECT * FROM {0} WHERE dt > '2015-01-01' AND dt < '2015-05-23';".format(tname)
     #sql = "SELECT * FROM {0} WHERE dt > '2015-01-01' AND dt < now();".format(tname)
     #sql = "SELECT * FROM {0} WHERE dt BETWEEN '2015-01-01' AND dt < now();".format(tname)
     #sql = "SELECT count(*) FROM {0} WHERE dt > '2015-01-01' AND dt < '2015-05-23';".format(tname)
