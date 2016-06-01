ca8c9f1c1f2315cce2258ed36cf5bfe7a579e03a
Chandra Krintz <ckrintz@yahoo.com>
2016-06-01 10:18:21 -0700

    minor clean up

--- process_output_files/process.py
+++ process_output_files/process.py
@@ -13,7 +13,6 @@ def main():
     parser = argparse.ArgumentParser(description='CSV File Processing')
     #required arguments
     parser.add_argument('fname',action='store',help='CSV file (must have header row) to import into DB')
-    parser.add_argument('outfname',action='store',help='text file to output for plotting')
     #optional arguments
     parser.add_argument('--overWrite',action='store_true',default=False,help='overwrite entire db table with data in this CSV (default is to append, skipping keys that exists (date,time,ID))')
     parser.add_argument('--debug',action='store_true',default=False,help='Turn debugging on')
@@ -66,28 +65,6 @@ def main():
         print row
     db.closeConnection()
     
-    '''
-    #open the csv file for writing out dataset for plotting: datetime loc1_temp loc2_temp ... loc9_temp
-    locs = ["Lisque","Figueroa","Windmill","Main","BoneT","BoneH","NE","Vulture","Blue"]
-    with open(args.outfname,'wt') as fout:
-        with open(args.fname,'rt') as f:
-            csvFile = csv.DictReader(f) #create a dictionary per row with headers as keys
-            for row in csvFile:
-                print row
-	        #{'temp': '51', 'flash': 'NoFlash', 'bad_temp': 'False', 'box_path:filename': 'Bone\\2015\\09\\16:BoneH_2015-09-16_22:30:48_1499.JPG', 'time': '22:30:48', 'date': '2015-09-16', 'orig_fname': 'RCNX1499.JPG', 'ID': '1499', 'size': '1700891'}
-		#extract filename
-		this_prefix = row['box_path:filename'].split(':')[1]
-		#extract prefix from filename
-		this_loc = this_prefix.split('_')[0]
-		    
-		#dt: {loc: value}  however there may be multiple dt's for a loc
-			
-                temp = row['temp']
-		ts = '{0} {1}'.format(row['date'],row['time'])
-		dt = datetime.strptime(ts, '%Y-%M-%D %H:%M:%S')
-		dtdict[dt] = temp
-	
-    '''
 
 if __name__ == '__main__':
     main()
