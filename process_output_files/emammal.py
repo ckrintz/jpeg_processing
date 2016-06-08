cd7414c26ddf0a77fc12260fc4dff6f51f6f5d50
ckrintz <ckrintz@yahoo.com>
2016-06-07 18:42:28 -0700

    bug fixes from emammal comparison

--- process_output_files/emammal.py
+++ process_output_files/emammal.py
@@ -21,27 +21,31 @@ SEDGWICK='7411611121' #Shared Sedgwick Images Folder
 def generate_emammal_Deployment(deployID): #only called once
     with open('Deployment.csv','wt') as f:
         csvFile = csv.writer(f)
-        csvFile.writerow((deployID,'Longitude Resolution','Latitude Resolution','Camera Deployment Begin Date','Camera Deployment End Date','Bait Type','Bait Description','Feature Type','Feature Type methodology','Camera Id','Quiet Period Setting (in seconds)','Restriction on Access (Yes/No only)','Camera Failure Details',' ',' ',' ',' '))
-        csvFile.writerow(('Main_Reconyx_001','-120.036208','34.719785','2013/07/13 15:30:00','2013/10/13 22:45:32','Water','Water Hole','Trail,game','Tree mount','Reconyx_001','0','Yes','Functioning',' ',' ',' ',' '))
+        #csvFile.writerow((deployID,'Longitude Resolution','Latitude Resolution','Camera Deployment Begin Date','Camera Deployment End Date','Bait Type','Bait Description','Feature Type','Feature Type methodology','Camera Id','Quiet Period Setting (in seconds)','Restriction on Access (Yes/No only)','Camera Failure Details','','','',''))
+        csvFile.writerow(('Camera Deployment ID','Camera Site Name','Camera Deployment Begin Date','Camera Deployment End Date','Actual Latitude','Actual Longitude','Camera Failure Details','Bait  ','Bait Description','Feature','Feature Methodology','Camera ID','Quiet Period Setting','Sensitivity Setting'))
+        csvFile.writerow(('Main_Reconyx_001','Main Road Water Hole','2013/07/13 15:30:00','2013/10/13 22:45:32','34.71995','-120.0363','Camera Functioning','No bait','','Water source/spring','water hole','Reconyx_001','0',''))
 	#Deployment ID: Main_Reconyx_001 must match in Images.csv
 
 def generate_emammal_Project(projID,projName,start_date_string):
     with open('Project.csv','wt') as f: #only called once
         csvFile = csv.writer(f)
-        csvFile.writerow(('Project Id','Publish Date','Project Name','Project Objectives','Project Owner (organization or individual)','Project Owner Email (if applicable)','Principal Investigator','Principal Investigator Email','Project Contact','Project Contact Email','Country Code','Project Data Use and Constraints',' ',' ',' '))
-        csvFile.writerow((projID,start_date_string,projName,'Camera trap survey','Sedgwick Reserve','sedgwick@lifesci.ucsb.edu','Chandra Krintz','ckrintz@cs.ucsb.edu','Chandra Krintz','ckrintz@cs.ucsb.edu','840','None',' ',' ',' '))
+        #csvFile.writerow(('Project Id','Publish Date','Project Name','Project Objectives','Project Owner (organization or individual)','Project Owner Email (if applicable)','Principal Investigator','Principal Investigator Email','Project Contact','Project Contact Email','Country Code','Project Data Use and Constraints','','',''))
+        csvFile.writerow(('Project ID','Publish Date','Project Name','Project Objectives','Project Owner','Project Owner Email','Principal Investigator','Principal Investigator Email','Project Contact','Project Contact Email','Project Latitude','Project Longitude','Country Code','Project Data Use and Constraints'))
+        csvFile.writerow((projID,start_date_string,projName,'Camera trap survey','Sedgwick Reserve','sedgwick@lifesci.ucsb.edu','Chandra Krintz','ckrintz@cs.ucsb.edu','Chandra Krintz','ckrintz@cs.ucsb.edu','34.6928059','-120.0406417','USA','No constraints'))
 
 def generate_emammal_Image(deployID,seqID,iID,path,tss,writeHeader=False):
     #called once for each image
     if writeHeader:
         with open('Image.csv','wt') as f: 
             csvFile = csv.writer(f)
-            csvFile.writerow(('Deployment ID','Sequence ID','Image Id','Location','Photo Type','Photo Type Identified by','Genus Species','IUCN Identification Number','Date_Time Captured', 'Age', 'Sex', 'Individual ID','Count','Animal recognizable (Y/N, blank)','individual Animal notes',' ',' '))
-            csvFile.writerow((deployID,seqID,iID,path,'Animal',' ','Unknown Animal','2',tss, ' ', ' ', ' ',' ',' ',' ',' ',' '))
+            #csvFile.writerow(('Deployment ID','Sequence ID','Image Id','Location','Photo Type','Photo Type Identified by','Genus Species','IUCN Identification Number','Date_Time Captured', 'Age', 'Sex', 'Individual ID','Count','Animal recognizable (Y/N, blank)','individual Animal notes','',''))
+            csvFile.writerow(('Deployment ID','Image Sequence ID','Image ID','Location','Image File Name','Photo Type', 'Photo Type Identified by','Genus Species','IUCN ID','IUCN Status','Date_Time','Interest Rank','Age','Sex','Individual ID','Count','Animal recognizable','Individual Animal Notes','Digital Origin','Embargo Period','Restrictions on Access','Image Use Restrictions'))
+            #csvFile.writerow((deployID,seqID,iID,path,'Animal','','Unknown Animal','2',tss,'','','','','','','',''))
+            csvFile.writerow((deployID,seqID,iID,path,iID,'','','','','',tss,'','','','','','','','born digital','','',''))
     else:
         with open('Image.csv','at') as f: 
             csvFile = csv.writer(f)
-            csvFile.writerow((deployID,seqID,iID,path,'Animal',' ','Unknown Animal','2',tss, ' ', ' ', ' ',' ',' ',' ',' ',' '))
+            csvFile.writerow((deployID,seqID,iID,path,iID,'','','','','',tss,'','','','','','','','born digital','','',''))
 
 def generate_emammal_Sequence(deployID,seqID,tss_start,tss_end,writeHeader=False):
     '''tss_start and tss_end are string datetime format: YYYY/MM/DD HH/mm/ss'''
@@ -51,12 +55,13 @@ def generate_emammal_Sequence(deployID,seqID,tss_start,tss_end,writeHeader=False
     if writeHeader:
         with open('Sequence.csv','wt') as f: 
             csvFile = csv.writer(f)
-            csvFile.writerow(('Observation Type','Deployment ID','Image Sequence ID','Begin Sequence Date Time','End Sequence Date Time','Species Name','Species Common Name','Age','Sex','Individual ID','Count','Animal recognizable (Y/N)','Individual Animal Notes','TSN ID','IUCN ID',' ',' '))
-            csvFile.writerow(('Volunteer',deployID,seqID,tss_start,tss_end,'Unknown Animal','Unknown Animal',' ',' ',' ',' ',' ',' ','2','2',' ',' '))
+            #csvFile.writerow(('Observation Type','Deployment ID','Image Sequence ID','Begin Sequence Date Time','End Sequence Date Time','Species Name','Species Common Name','Age','Sex','Individual ID','Count','Animal recognizable (Y/N)','Individual Animal Notes','TSN ID','IUCN ID','',''))
+            csvFile.writerow(('Observation Type','Deployment ID','Image Sequence ID','Date_Time Begin','Date_Time End','Genus species','Species Common Name','Age','Sex','Individual ID','Count','Animal recognizable','Individual Animal Notes','TSN ID','IUCN ID','IUCN Status'))
+            csvFile.writerow(('Volunteer',deployID,seqID,tss_start,tss_end,'','','','','','','','','','',''))
     else:
         with open('Sequence.csv','at') as f: 
             csvFile = csv.writer(f)
-            csvFile.writerow(('Volunteer',deployID,seqID,tss_start,tss_end,'Unknown Animal','Unknown Animal',' ',' ',' ',' ',' ',' ','2','2',' ',' '))
+            csvFile.writerow(('Volunteer',deployID,seqID,tss_start,tss_end,'','','','','','','','','','',''))
 
 ############## END generate eMammal CSVs -- see ../emammal/CJKREADME for sample ################
 
@@ -115,6 +120,7 @@ def main():
     parser.add_argument('--s3sec',default=None,action='store',help='AWS S3_SECRET_KEY')
     parser.add_argument('--s3bkt',default=None,action='store',help='AWS S3 Bucket Name')
     parser.add_argument('--uploadOnly',default=None,action='store',help='Local file name in current working directory to upload to S3. This is used if the original run has exceptions with the s3 upload (which then writes the file locally).  Note the file names from this problem run and run this program for each individual name to upload.')
+    parser.add_argument('--uploadCSVsOnly',default=False,action='store_true',help='Generate and upload CSV files (metainfo) only to S3 (requires s3acc,s3sec,s3bkt)')
     parser.add_argument('--debug',action='store_true',default=False,help='Turn debugging on')
     args = parser.parse_args()
 
@@ -124,20 +130,24 @@ def main():
     acc = args.s3acc
     sec = args.s3sec
     bkt = args.s3bkt
+    csvonly = args.uploadCSVsOnly
     upload = args.uploadOnly
     first = True
+    if csvonly and upload is not None:
+        print 'Error, uploadOnly and uploadCSVsOnly cannot both be set at the same time'
+        sys.exit(1)
     if acc and sec and bkt and upload is not None:
         upload_file_to_s3(upload,bkt,acc,sec,isText=(upload.endswith('.csv')))
         sys.exit(0)
-    if upload is not None:
+    if not csvonly and upload is not None:
         print 'Error, uploadOnly must be set with all three s3 arguments for upload to be performed'
         sys.exit(1)
 
     #set the name of the file prefix to find/search from
     #fname = 'Blue_2015-04-15_14:34:10_00'
-    #fname = 'Main_2013-10-13_02' #30 in dir at 450K each is 7.3MB
+    fname = 'Main_2013-10-13_02' #30 in dir at 450K each is 7.3MB
     #fname = 'Sedgwick Camera' 
-    fname = 'Main_2013-10-13' #350 in dir at 450K each is 161MB
+    #fname = 'Main_2013-10-13' #350 in dir at 450K each is 161MB
 
     #log into box if needed
     auth_client = upload_files.setup()
@@ -153,7 +163,7 @@ def main():
     #write deployment metainfo to the Deployment CSV
     generate_emammal_Deployment(deployID)
     #write project metainfo to the Project CSV
-    generate_emammal_Project(projPrefix,'2016/06/05',projPrefix)
+    generate_emammal_Project(projPrefix,projPrefix,'2016/06/05')
     writeSequenceHeader = True  #write out header the first time only
     writeImageHeader = True  #write out header the first time only
 
@@ -247,8 +257,9 @@ def main():
         
         #download file from Box and write it to s3
         if acc and sec and bkt:
-            fileObj = upload_files.get_file(auth_client,ids[name]) #get the Box File object from the entity ID
-            upload_file_to_s3(name,bkt,acc,sec,fileIsString=fileObj.content())
+	    if not csvonly:
+                fileObj = upload_files.get_file(auth_client,ids[name]) #get the Box File object from the entity ID
+                upload_file_to_s3(name,bkt,acc,sec,fileIsString=fileObj.content())
 
     #at this point all csv files are written.  Upload them to s3
     if acc and sec and bkt:
