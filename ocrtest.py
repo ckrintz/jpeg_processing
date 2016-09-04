f98e431414d293d0631da86a4113d0842a460d66
Chandra Krintz <ckrintz@cs.ucsb.edu>
2016-09-04 11:36:51 -0700

    ocr by resolution

--- ocrtest.py
+++ ocrtest.py
@@ -39,17 +39,28 @@ def get_exif(fn,pictype):
         stop_tag = 'EXIF ExifImageWidth'
         wid_tag = vars(tags[stop_tag])['printable']
         print 'Filename: {0}, datetime {1}, res {2}x{3}, pictype {4}'.format(fn,dt_tag,wid_tag,len_tag,pictype)
+	try:
+	    if int(wid_tag) == 2560 and int(len_tag) == 1920: #Windmill1, UpperRes no temp
+		temp = -9998 #no temp
+	    elif int(wid_tag) == 3264 and int(len_tag) == 2448: #Windmill1 2014, BoneT
+                temp = crop_and_recognize.run_c1(fname, 'ocr_knn/flask_ocr/backend/data/data_files/camera_1/')
+	    elif int(wid_tag) == 1920 and int(len_tag) == 1080: #main,lisque,fig,ne,vulture_pre16
+                    temp = crop_and_recognize.run_c2(fname, 'ocr_knn/flask_ocr/backend/data/data_files/camera_2/')
+	    elif int(wid_tag) == 3776 and int(len_tag) == 2124: #boneH
+                    temp = crop_and_recognize.run_c3(fname, 'ocr_knn/flask_ocr/backend/data/data_files/camera_3/')
+	    elif int(wid_tag) == 2688 and int(len_tag) == 1512: #vulture16
+		    temp = -9997 #TBD rerun these when supported
+		    err = True #error
+	    elif int(wid_tag) == 2048 and int(len_tag) == 1536: #windmillG,blue
+		    temp = -9997 #TBD rerun these when supported
+	    else:
+		    print 'Error: unsupported OCR resolution: {0}x{1}'.format(wid_tag,len_tag)
+		    temp = -9996
+        except Exception as e:
+	    print e
+	    print 'Error: OCR exception for: {0}x{1}'.format(wid_tag,len_tag)
+	    temp = -9995
 
-        if pictype == 1:
-            temp = crop_and_recognize.run_c1(fullfname, 'ocr_knn/flask_ocr/backend/data/data_files/camera_1/')
-        elif pictype == 2:
-            temp = crop_and_recognize.run_c2(fullfname, 'ocr_knn/flask_ocr/backend/data/data_files/camera_2/')
-        elif pictype == 3:
-            temp = crop_and_recognize.run_c3(fullfname, 'ocr_knn/flask_ocr/backend/data/data_files/camera_3/')
-        else:
-	    print 'Error unknown pictype: {0}'.format(pictype)
-	    temp = -9999
-        
         print 'temp {0}'.format(temp)
 
 def main():
